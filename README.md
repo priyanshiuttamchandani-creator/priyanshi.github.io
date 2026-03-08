ebd86b55b7de65bd3dc58cb17598ccb3c0f41531
priyanshiuttamchandani-creator <priyanshiuttamchandani@gmail.com>
2026-03-08 22:31:21 +0530

    Update and rename README.md to python.py

--- README.md
+++
@@ -1,1 +0,0 @@
-# priyanshi.github.io
---
+++ python.py
@@ -0,0 +1,6 @@
+//Program to add two numbers provided by the user
+
+num1=int(input("Enter the number: "))
+num2=int(input("Enter the number:  "))
+num3=num1+num2
+print("Addition is :  ",num3)
